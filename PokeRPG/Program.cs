7749a786575fbf69811f4a373c575734f1a2334c
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-08-22 08:17:57 +0700

    a

--- PokeRPG/Program.cs
+++ PokeRPG/Program.cs
@@ -24,7 +24,7 @@ public class Program
 		_client.Log += Log;
 
 		await _client.LoginAsync(TokenType.Bot,
-			"token");
+			"t");
 		await _client.StartAsync();
 
 		_client.MessageReceived += MessageReceived;
